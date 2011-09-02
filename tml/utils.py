4a442160abb840763ccad94ca3fddf482d31eda2
SushiTee <weichel.sascha@xxx.xx>
2011-09-02 03:02:21 +0200

    fixed string_to_ints fixed ints_to_string (by bnn)

--- tml/utils.py
+++ tml/utils.py
@@ -18,14 +18,18 @@ def int32(x):
             return -2147483648
     return x
 
-def ints_to_string(num):
-    string = ''
-    for val in num:
-        string += chr(max(0, min(((val>>24)&0xff)-128, 255)))
-        string += chr(max(0, min(((val>>16)&0xff)-128, 255)))
-        string += chr(max(0, min(((val>>8)&0xff)-128, 255)))
-        string += chr(max(0, min((val&0xff)-128, 255)))
-    return string.partition('\x00')[0]
+def save_ord(char):
+    num = ord(char)
+    if num > 0x7F:
+        num = 0x100 - num
+        if num < 128:
+            return -num
+        else:
+            return -128
+    return num
+
+def safe_chr(i):
+    return chr(i if i >= 0 else 256 + i)
 
 def string_to_ints(in_string, length=8):
     ints = []
@@ -36,6 +40,14 @@ def string_to_ints(in_string, length=8):
                 string += in_string[j]
             else:
                 string += chr(0)
-        ints.append(int32(((ord(string[0])+128)<<24)|((ord(string[1])+128)<<16)|((ord(string[2])+128)<<8)|(ord(string[3])+128)))
+        ints.append(int32(((save_ord(string[0])+128)<<24)|((save_ord(string[1])+128)<<16)|((save_ord(string[2])+128)<<8)|(save_ord(string[3])+128)))
     ints[-1] &= int32(0xffffff00)
     return ints
+
+def ints_to_string(num):
+    return ''.join([''.join([
+        safe_chr(((val>>24)&0xff)-128),
+        safe_chr(((val>>16)&0xff)-128),
+        safe_chr(((val>>8)&0xff)-128),
+        safe_chr((val&0xff)-128),
+    ]) for val in num]).partition('\x00')[0]
